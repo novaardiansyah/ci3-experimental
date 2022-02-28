393e30ee9c573dc25fbe542fbda0c2f19ff58a4e
novaardiansyah <novaardiansyah78@gmail.com>
2022-02-28 10:39:02 +0700

    configuration plugin admin lte
    - create a new page to login and register

--- application/modules/auth/controllers/Auth.php
+++ application/modules/auth/controllers/Auth.php
@@ -5,6 +5,27 @@ class Auth extends CI_Controller
 {
 	public function index()
 	{
-		echo "Hello World";
+		$data = [
+			'title' => 'Login',
+			'content' => 'auth',
+			'page' => 'login-page',
+		];
+
+		$this->load->view('auth/layouts/header', $data);
+		$this->load->view('auth/login');
+		$this->load->view('auth/layouts/footer');
+	}
+
+	public function register()
+	{
+		$data = [
+			'title' => 'Register',
+			'content' => 'auth/register',
+			'page' => 'register-page',
+		];
+
+		$this->load->view('auth/layouts/header', $data);
+		$this->load->view('auth/register');
+		$this->load->view('auth/layouts/footer');
 	}
 }
